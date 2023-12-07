ccc207f34046698f408eb642743a0a3df85a0b70
ed55567 <edblade555@gmail.com>
2023-12-07 09:25:56 -0500

    add chromactic test paragraph

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello, this is a chromatic test.</p>
+        <p>To see if this works for a big project</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
