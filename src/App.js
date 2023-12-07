fdffe6a54b4ce7623df09e7ff5039d35c44cc22b
ed55567 <edblade555@gmail.com>
2023-12-07 09:27:51 -0500

    add chromactic test paragraph line 2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <p>Hello, this is a chromatic test.</p>
         <p>To see if this works for a big project</p>
+        <p>chromatic test 2 line has lots of moving parts</p>
       </header>
     </div>
   );
